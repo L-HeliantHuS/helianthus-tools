bb3d5aac1998bf56e09fd2ee571de6b945c58247
HeliantHuS <1984441370@qq.com>
2019-10-09 10:47:23 +0800

    Hello

---
+++ setup.py
@@ -0,0 +1,39 @@
+
+#!/usr/bin/env python3
+# coding=utf-8
+ 
+from setuptools import setup, find_packages
+ 
+__author__ = 'HeliantHuS'
+__date__ = '2019/10/09'
+ 
+setup(
+    name='helianthus_tools',
+    version='1.4',
+    description=(
+        'HeliantHuS-Tools ~ wow awesome!'
+    ),
+    long_description=open('README.txt').read(),
+    author='HeliantHuS',
+    author_email='1984441370@qq.com',
+    maintainer='HeliantHuS',
+    maintainer_email='1984441370@qq.com',
+    license='MIT License',
+    packages=find_packages(),
+    platforms=["all"],
+    url='https://github.com/L-HeliantHuS/helianthus-tools',
+    install_requires=['requests', 'paramiko'],
+    include_package_data=True,
+    classifiers=[
+        'Operating System :: OS Independent',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Topic :: Software Development :: Libraries'
+    ],
+)
